e48c83f5c6a7ae877635bd7e1e1e89dcf2597f8b
Vishal Thakur <nvisu98@gmail.com>
2026-01-06 14:49:07 +0530

    logger deleted

--- logger.py
+++
@@ -1,6 +0,0 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-logger.info("Logger is set up.")
-logger.info("This is an info message.")
-logger.error("This is an error message.")
